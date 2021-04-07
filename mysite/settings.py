2d8b0d470e21aec52d794eb75bf3fb14c0dcaa8f
UAACC <dongheng@ualberta.ca>
2021-04-07 13:00:26 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [
