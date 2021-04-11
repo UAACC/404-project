ea9f4bcdd802620f2bc48fc38d91ba354af95685
UAACC <dongheng@ualberta.ca>
2021-04-10 22:05:40 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd1q6l3015ichi7',#database name
-        'USER': 'zmttwpsvbwdsib', #username
-        'PASSWORD': 'c018480df07dace4b758315168b95bc80ec39f273acd4ef0ead8444043029755', 
+        'NAME': 'devs33qk1q3ahj',#database name
+        'USER': 'xmqbwtlqfejlkf', #username
+        'PASSWORD': '29c6780712c7fb9b8a2ac819eab6bdd9a1ad5497504a7ef2491f3b674f947765', 
         'HOST': 'ec2-52-21-153-207.compute-1.amazonaws.com', 
         'PORT': '5432',  
     }
