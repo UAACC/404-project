3cf42a5ada3c794676b83a6a908394459ce75399
Kay7777 <kay7song@gmail.com>
2021-04-11 23:16:05 -0600

    add inbox

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Node, Author, Post, Comment, Likes, FriendRequest
+from .models import Node, Author, Post, Comment, Likes, FriendRequest, Inbox
 admin.site.register(Node)
 admin.site.register(Author)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Likes)
 admin.site.register(FriendRequest)
+admin.site.register(Inbox)
